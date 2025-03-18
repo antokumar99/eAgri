e705e7411273cd16af291c061a87a617b5ad0ce8
unknown <antokumar00@gmail.com>
2025-03-18 11:04:16 +0000

    Routes Modify

--- Backend/routes/combinedRoutes.js
+++ Backend/routes/combinedRoutes.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const AuthController = require('../controllers/AuthController');
 const UserController = require('../controllers/UserController');
+const ResearchController = require('../controllers/ResearchController');
 const authMiddleware = require('../middleware/authMiddleware');
+const adminController = require('../controllers/AdminController');
 
 // Authentication routes
 router.post('/login', AuthController.login);
@@ -11,9 +13,17 @@ router.get('/verify-email/:token', AuthController.verifyEmail);
 router.post('/resend-verification', AuthController.resendVerification);
 // router.get('/verify-email/:token', AuthController.verifyEmail);
 
+// Admin routes
+// router.post('/admin/register', adminController.registerAdmin);
+// router.use(authMiddleware);
+// router.post('/admin/login', adminController.loginAdmin);
 
 // User routes
 // router.get('/profile', UserController.getProfile);
-router.get('/profile', authMiddleware, UserController.getProfile);
+router.get('/profile', authMiddleware, UserController.getProfile);             
+
+// Research routes
+router.get('/research', ResearchController.getAllResearch);
+router.post('/research', ResearchController.addResearch);
 
 module.exports = router;
