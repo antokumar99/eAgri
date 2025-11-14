f1e8d263d956479d0d94179bfe54f5b1cab4ec47
miraz <fmsmiraz@gmail.com>
2025-11-14 11:25:55 +0000

    Security: Remove API keys from git tracking and add env template

---
+++ eAgri/config/env.example.js
@@ -0,0 +1,15 @@
+// Environment configuration template
+// Copy this file to env.js and add your actual API keys
+// DO NOT commit env.js to git
+
+const ENV_CONFIG = {
+  // Get your API key from https://openweathermap.org/api_keys
+  OPENWEATHER_API_KEY: 'YOUR_OPENWEATHER_API_KEY_HERE',
+  
+  // You can add other sensitive configs here
+  // FIREBASE_API_KEY: 'your_firebase_key',
+  // GOOGLE_MAPS_API_KEY: 'your_google_maps_key',
+  // etc.
+};
+
+export default ENV_CONFIG;
